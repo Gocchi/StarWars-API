a1f50f5a8e58aa3d87a4f04db5f086f16ae78dda
ilse centeno <centenoilse93@gmail.com>
2017-06-12 10:16:13 -0500

    metodo GET

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ var cargarPagina=function(){
 };
 
 var cargarPersonajes=function(){
-	$.ajax("https://swapi.co/api/people/",{
-		method:'GET',
-		dataType:'json',
-		success:function(response){
-		var personajes=response.results;
-		var $ul=$("#personajes");
-		personajes.forEach(function(personaje){
-			var $li=$("<li />");
-			$li.text(personaje.name);
-			$ul.append($li);
-		});
-		},
-		error: function(error){
-			console.log("error", error);
-		}
-	});
+	var url='https://swapi.co/api/people/';
+	$.get(url, function(response){
+	console.log(response);
+	var personajes=response.results;
+	var total=response.count;
+	mostrarTotalPersonajes(total);
+	mostrarPersonajes(personajes);
+});
+};
+var mostrarTotalPersonajes = function(total) {
+    $('#total').text(total);
 };
 
+var mostrarPersonajes = function(personajes) {
+    
+    personajes.forEach(function(personaje) {
+              var $li = $("<li />");
+              var $ul = $('#personajes');
+              
+         $ul.append($li);
+          $li.text(personaje.name);
+              console.log(personaje.name);
+          });
+    
+};
 $(document).ready(cargarPagina);
